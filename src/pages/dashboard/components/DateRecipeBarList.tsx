093113839b49abb90781d9c2243c61aaabcfe287
anpdt <sajno076@gmail.com>
2022-10-03 20:07:22 +0700

    adjust layout

--- src/pages/dashboard/components/DateRecipeBarList.tsx
+++ src/pages/dashboard/components/DateRecipeBarList.tsx
@@ -40,7 +40,7 @@ type Props = {
 
 const DateRecipeBarList = ({ recipes }: any) => {
   return (
-    <Stack sx={{ height: "410px", overflow: "scroll" }}>
+    <Stack sx={{ height: "400px", overflowY: "scroll" }}>
       {recipes.map((recipe: any) => (
         <DateRecipeMenuItem direction="row" key={recipe.food.id}>
           <Stack className="date-recipe-info">
