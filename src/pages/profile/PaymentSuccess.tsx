9c9ed6e2b6df3c965ca83a3303e4ce8f4fcb94b5
An <sajno076@gmail.com>
2022-10-12 11:54:08 +0700

    style it

--- src/pages/profile/PaymentSuccess.tsx
+++ src/pages/profile/PaymentSuccess.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography, Button } from "@mui/material";
+import { Paper, Typography, Button, Stack } from "@mui/material";
 import { BsCartCheckFill } from "react-icons/bs";
 import { useSearchParams, Link } from "react-router-dom";
 import { formatPrice } from "utils/formatPrice";
@@ -8,16 +8,24 @@ const PaymentSuccess = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   let user = useSelector((state: any) => state.auth.auth?.user);
   return (
-    <Stack justifyContent="center" sx={{ fontSize: "40px" }}>
-      <BsCartCheckFill />
-      <Typography variant="h4">
-        Bạn đã nạp thành công{" "}
-        {formatPrice(Number(searchParams.get("searchParams")))}
-      </Typography>
-      <Link to={`users/${user.id}`}>
-        <Button variant="contained">Quay lại thông tin cá nhân</Button>
-      </Link>
-    </Stack>
+    <Paper
+      elevation={2}
+      sx={{
+        textAlign: "center",
+        "& .pay-icon": { color: "#1AC073", fontSize: "40px" },
+      }}
+    >
+      <Stack spacing={2}>
+        <BsCartCheckFill className="pay-icon" />
+        <Typography variant="h5">
+          Bạn đã nạp thành công{" "}
+          {formatPrice(Number(searchParams.get("searchParams")))}
+        </Typography>
+        <Link to={`users/${user.id}`}>
+          <Button variant="contained">Quay lại thông tin cá nhân</Button>
+        </Link>
+      </Stack>
+    </Paper>
   );
 };
 
